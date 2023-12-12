012ffa2e7ec51de3e269dd786199ce3fb326a46c
Fuwn <contact@fuwn.me>
2023-12-12 08:23:38 +0000

    style: use rustfmt

--- src/html.rs
+++ src/html.rs
@@ -21,9 +21,7 @@ fn link_from_host_href(url: &Url, href: &str) -> Option<String> {
   Some(format!(
     "gemini://{}{}{}",
     url.domain()?,
-    {
-      if href.starts_with('/') { "" } else { "/" }
-    },
+    { if href.starts_with('/') { "" } else { "/" } },
     href
   ))
 }
