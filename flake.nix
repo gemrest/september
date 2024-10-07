4bfd31c84240b5e7ad0d719ded00838e1ab9c8fd
Fuwn <contact@fuwn.me>
2024-10-06 17:02:20 -0700

    chore(flake): add packages and apps attributes

--- flake.nix
+++ flake.nix
@@ -1,7 +1,15 @@
 {
+  description = "Simple & Efficient Gemini-to-HTTP Proxy";
+
   inputs = {
+    crane.url = "github:ipetkov/crane";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -11,46 +19,95 @@
 
   outputs =
     {
-      nixpkgs,
+      crane,
       flake-utils,
+      nixpkgs,
       rust-overlay,
+      self,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
+        pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [ (import rust-overlay) ];
+        };
+
+        craneLib = crane.mkLib pkgs;
+
+        meta = with pkgs.lib; {
+          homepage = "https://github.com/gemrest/september";
+          description = "Simple & Efficient Gemini-to-HTTP Proxy";
+          license = licenses.gpl3Only;
+          maintainers = [ maintainers.Fuwn ];
+          mainPackage = "september";
+          platforms = platforms.linux;
+        };
+
+        september = craneLib.buildPackage {
+          inherit meta;
+
+          strictDeps = true;
+
+          src = pkgs.lib.cleanSourceWith {
+            src = ./.;
+
+            filter =
+              path: type:
+              builtins.match ".*css$" path != null
+              || builtins.match ".*\\.git.*" path != null
+              || (craneLib.filterCargoSources path type);
+          };
+        };
       in
       {
-        devShells.${system} =
+        packages = {
+          inherit september;
+
+          default = self.packages.${system}.september;
+        };
+
+        apps = {
+          september = {
+            inherit meta;
+
+            type = "app";
+            program = "${self.packages.${system}.september}/bin/september";
+          };
+
+          default = self.apps.${system}.september;
+        };
+
+        devShells.default =
           with pkgs;
           mkShell.override
             {
               stdenv = stdenvAdapters.useMoldLinker clangStdenv;
             }
             {
+              RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
               buildInputs = [
-                # https://github.com/oxalica/rust-overlay/issues/136#issuecomment-1716902831
+                cargo-make
+                openssl
+                pkg-config
+                cargo-watch
+
                 (lib.hiPrio (
                   rust-bin.stable.latest.minimal.override {
                     extensions = [ "rust-docs" ];
                   }
                 ))
+
                 (rust-bin.selectLatestNightlyWith (
                   toolchain:
                   toolchain.minimal.override {
                     extensions = [ "rustfmt" ];
                   }
                 ))
-                cargo-make
-                openssl
-                pkg-config
-                cargo-watch
               ];
-
-              # https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/5?u=fuwn
-              RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
             };
       }
     );
