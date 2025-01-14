d3b9c1cded5530ba20770702cfaab83f1a4c2e2e
Fuwn <contact@fuwn.me>
2025-01-14 00:06:15 -0800

    refactor(url): simplify approach

--- src/url.rs
+++ src/url.rs
@@ -5,61 +5,55 @@ pub fn from_path(
   fallback: bool,
   configuration: &mut crate::response::configuration::Configuration,
 ) -> Result<Url, url::ParseError> {
-  Ok(
-    #[allow(clippy::blocks_in_conditions)]
-    match Url::try_from(&*if path.starts_with("/proxy") {
-      configuration.set_proxy(true);
+  Url::try_from(&*if path.starts_with("/proxy") {
+    configuration.set_proxy(true);
 
-      format!(
-        "gemini://{}{}",
-        path.replace("/proxy/", ""),
-        if fallback { "/" } else { "" }
-      )
-    } else if path.starts_with("/x") {
-      configuration.set_proxy(true);
+    format!(
+      "gemini://{}{}",
+      path.replace("/proxy/", ""),
+      if fallback { "/" } else { "" }
+    )
+  } else if path.starts_with("/x") {
+    configuration.set_proxy(true);
 
-      format!(
-        "gemini://{}{}",
-        path.replace("/x/", ""),
-        if fallback { "/" } else { "" }
-      )
-    } else if path.starts_with("/raw") {
-      configuration.set_proxy(true);
-      configuration.set_raw(true);
+    format!(
+      "gemini://{}{}",
+      path.replace("/x/", ""),
+      if fallback { "/" } else { "" }
+    )
+  } else if path.starts_with("/raw") {
+    configuration.set_proxy(true);
+    configuration.set_raw(true);
 
-      format!(
-        "gemini://{}{}",
-        path.replace("/raw/", ""),
-        if fallback { "/" } else { "" }
-      )
-    } else if path.starts_with("/nocss") {
-      configuration.set_proxy(true);
-      configuration.set_no_css(true);
+    format!(
+      "gemini://{}{}",
+      path.replace("/raw/", ""),
+      if fallback { "/" } else { "" }
+    )
+  } else if path.starts_with("/nocss") {
+    configuration.set_proxy(true);
+    configuration.set_no_css(true);
 
-      format!(
-        "gemini://{}{}",
-        path.replace("/nocss/", ""),
-        if fallback { "/" } else { "" }
-      )
-    } else {
-      format!(
-        "{}{}{}",
-        {
-          std::env::var("ROOT").unwrap_or_else(|_| {
-            warn!(
-              "could not use ROOT from environment variables, proceeding with \
-               default root: gemini://fuwn.me"
-            );
+    format!(
+      "gemini://{}{}",
+      path.replace("/nocss/", ""),
+      if fallback { "/" } else { "" }
+    )
+  } else {
+    format!(
+      "{}{}{}",
+      {
+        std::env::var("ROOT").unwrap_or_else(|_| {
+          warn!(
+            "could not use ROOT from environment variables, proceeding with \
+             default root: gemini://fuwn.me"
+          );
 
-            "gemini://fuwn.me".to_string()
-          })
-        },
-        path,
-        if fallback { "/" } else { "" }
-      )
-    }) {
-      Ok(url) => url,
-      Err(e) => return Err(e),
-    },
-  )
+          "gemini://fuwn.me".to_string()
+        })
+      },
+      path,
+      if fallback { "/" } else { "" }
+    )
+  })
 }
