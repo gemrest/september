80d356c79f40c89c81852936b2cd6a364509529a
Fuwn <contact@fuwn.me>
2025-06-02 13:02:18 +0000

    chore: Bump MSRV

--- src/response/configuration.rs
+++ src/response/configuration.rs
@@ -15,9 +15,13 @@ impl Configuration {
 
   pub const fn is_no_css(&self) -> bool { self.is_no_css }
 
-  pub fn set_proxy(&mut self, is_proxy: bool) { self.is_proxy = is_proxy; }
+  pub const fn set_proxy(&mut self, is_proxy: bool) {
+    self.is_proxy = is_proxy;
+  }
 
-  pub fn set_raw(&mut self, is_raw: bool) { self.is_raw = is_raw; }
+  pub const fn set_raw(&mut self, is_raw: bool) { self.is_raw = is_raw; }
 
-  pub fn set_no_css(&mut self, is_no_css: bool) { self.is_no_css = is_no_css; }
+  pub const fn set_no_css(&mut self, is_no_css: bool) {
+    self.is_no_css = is_no_css;
+  }
 }
